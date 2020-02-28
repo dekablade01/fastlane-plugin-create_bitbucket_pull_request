d5e3268dddfdcc62cd9190c5cc1370b7776d5190
Issarapong Poesua <dekablade01@gmail.com>
2020-02-28 22:51:48 +0700

    Initial Commit

---
+++ lib/fastlane/plugin/create_bitbucket_pull_request/version.rb
@@ -0,0 +1,5 @@
+module Fastlane
+  module CreateBitbucketPullRequest
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/create_bitbucket_pull_request_action_spec.rb
@@ -0,0 +1,9 @@
+describe Fastlane::Actions::CreateBitbucketPullRequestAction do
+  describe '#run' do
+    it 'prints a message' do
+      expect(Fastlane::UI).to receive(:message).with("The create_bitbucket_pull_request plugin is working!")
+
+      Fastlane::Actions::CreateBitbucketPullRequestAction.run(nil)
+    end
+  end
+end
