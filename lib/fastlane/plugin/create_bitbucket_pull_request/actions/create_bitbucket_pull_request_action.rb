dc4c156a097a3ebe329ff020c192b238bf383f58
Issarapong Poesua <dekablade01@gmail.com>
2020-02-29 15:58:21 +0700

    Create lane arguments

--- lib/fastlane/plugin/create_bitbucket_pull_request/actions/create_bitbucket_pull_request_action.rb
+++ lib/fastlane/plugin/create_bitbucket_pull_request/actions/create_bitbucket_pull_request_action.rb
@@ -26,11 +26,38 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "CREATE_BITBUCKET_PULL_REQUEST_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :head,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_HEAD',
+                                       description: 'The name of the branch where your changes are implemented',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :target,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_TARGET',
+                                       description: 'The name of the branch you want your changes pulled into',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :owner,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_REPOSITORY_OWNER',
+                                       description: 'The name of the repository owner (slug)',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :repository,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_REPOSITORY_NAME',
+                                       description: 'The name of the repository you want to submit the pull request to',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :title,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_TITLE',
+                                       description: 'The title of the pull request',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :description,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_DESCRIPTION',
+                                       description: 'The contents of the pull request',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :username,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_USERNAME',
+                                       description: 'The Bitbucket username (email)',
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       env_name: 'CREATE_BITBUCKET_PULL_REQUEST_PASSWORD',
+                                       description: 'The Bitbucket password',
+                                       type: String)
         ]
       end
 
